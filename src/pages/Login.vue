b894e1a024b16f474b8a5f1ed155b1108846d09c
Paula <pauladpm@outlook.com>
2018-09-12 01:56:37 -0300

    projeto teste - 1

---
+++ src/pages/Login.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .login
+    Background.background
+    LoginBox.login-box
+</template>
+
+<script>
+import Background from '@/components/Background'
+import LoginBox from '@/components/LoginBox'
+
+export default {
+  name: 'Login',
+  components: {
+    LoginBox,
+    Background
+  }
+}
+</script>
+
+<style scoped>
+
+.background {
+  color: blue;
+}
+.login-box {
+  align-content: center;
+}
+
+</style>
